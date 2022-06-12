5c40bb5e9207de145f0381a1b982559984bdb451
bernard flores <43194537+AbstractBern@users.noreply.github.com>
2022-06-12 16:33:37 -0500

    Update TT16_L6_Flores_Make1000Primes.py
    reduce lines of code

--- Python/assignment-projects/TT16_L6_Flores_Make1000Primes.py
+++ Python/assignment-projects/TT16_L6_Flores_Make1000Primes.py
@@ -2,35 +2,29 @@
 # COSC1336, Lab6
 # November 1, 2016
 # File, Output, Exceptions
+# Write the first 1000 primes into a text file.
 
-def main():
+# Prime is an imported file that contains the isPrime function to return 1=if prime, 0=if not prime
+from Prime import isPrime
+
+#def main():
+    number = 0
+    reached = 0
+    primes = 0
+    count = 0
     prime_file = open('1000Primes.txt', 'w')
     prime_file.write(format("The First 1,000 Primes", "^73"))
     prime_file.write("\n")
     prime_file.write(format("(the 1,000th is 7919)", "^73"))
     prime_file.write("\n\n")
-    prime_num = int(input('How many primes numbers do you want to find? :'))
-    n = 1000
-    test_num = 2
-    prime_count = 0
-    while prime_count < n:
-        is_prime = True
-        i = 2
-        while i < test_num:
-            if test_num % i == 0:
-                is_prime = False
-                break
-            else:
-                i += 1
-        if is_prime:
-            if prime_count%10 == 0 and prime_count != 0:
-                prime_file.write("\n")
-            prime_count += 1
-            prime_file.write(format(str(test_num), "7s") + " ")
-        test_num += 1
+    n = 1001
+    while not reached:
+        if isPrime(count):
+            prime_file.write(format(str(count), "7s") + " ")
+            primes += 1
+        elif primes >= n:
+            reached = 1
+        count += 1
     prime_file.close()
     print('Successful')
 main()
-# File will be in '1000Primes.txt'
-# Collaborated with Michael.
-#This makes alot of sense now.
