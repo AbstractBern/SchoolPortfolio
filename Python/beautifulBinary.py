8e371d9c61b5ddc17b026305736335730f2fcfe1
bernard flores <43194537+AbstractBern@users.noreply.github.com>
2022-06-20 15:37:55 -0500

    Delete beautifulBinary.py

--- Python/beautifulBinary.py
+++
@@ -1,5 +0,0 @@
-def beautifulBinaryString(b):
-    # remove occurences of '010' and return # of occurences
-    import re
-    result = [_.start() for _ in re.finditer('010', b)]
-    return len(result)
